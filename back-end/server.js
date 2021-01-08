23aceaa221969ca829c9a44b1704c4c26ab60155
andre <andrefmcastro94@gmail.com>
2021-01-08 01:48:35 +0000

    nodeJs Server
    added backend server, cors and express modules and created server.js file with server configuration

---
+++ back-end/server.js
@@ -0,0 +1,25 @@
+const host = process.env.HOST || "127.0.0.1";
+const port = process.env.PORT || "8080";
+
+const express = require("express");
+const app = express();
+
+app.use("/assets", express.static("assets"));
+app.use("views", express.static("views"));
+
+const cors = require("cors");
+app.use(cors());
+app.use(
+    cors({
+        exposedHeaders: ["Location"],
+    })
+);
+const permitedLinker = ["localhost", "127.0.0.1"];
+
+app.listen(port, function (err) {
+    if (!err) {
+        console.log("Your app is listening on " + host + " and port " + port);
+    } else {
+        console.log(err);
+    }
+});
